5103853d926bdab9a1470f972b032bd5c9aa24bb
eduardisrael <israel_pasaca@icloud.com>
2020-05-24 20:02:47 -0500

    Middleware, HTTP PUT, PATCH, DELETE

--- routes/libroRouter.js
+++ routes/libroRouter.js
@@ -9,7 +9,7 @@ function routes(Libro){
       const libro = new Libro(req.body);
 
       libro.save();
-      return res.status(201).json(libro); //Devuelva 
+      return res.status(201).json(libro); //Devuelva
     })
 
     .get((req, res) => {
@@ -25,30 +25,60 @@ function routes(Libro){
         return res.json(libros);
       });
     });
-
+  libroRouter.use('/libros/:idLibro', (req, res, next) => { //Middleware
+    Libro.findById(req.params.idLibro, (err,libro) =>{
+      if (err) {
+        return res.send(err);
+      }
+      if (libro){
+        req.libro = libro;
+        return next();
+      }
+      return res.sendStatus(404); //si no existe
+    });
+  });
   libroRouter.route('/libros/:idLibro') //5eb53471277a92fb671c72ad
-    .get((req,res) => {
-      Libro.findById(req.params.idLibro, (err,libro) =>{
-        if(err){
+    .get((req,res) => res.json(req.libro))
+    .put((req,res) => {
+        const { libro } = req;
+        libro.titulo = req.body.titulo;
+        libro.autor = req.body.autor;
+        libro.genero = req.body.genero;
+        libro.visto = req.body.visto;
+        req.libro.save((err) => {
+          if(err){
+            return res.send(err);
+          }
+          return res.json(libro);
+        });
+    })
+    .patch((req,res) => {
+      const { libro } = req;
+      // eslint-disable-next-line no-underscore-dangle
+      if(req.body._id){
+        // eslint-disable-next-line no-underscore-dangle
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        libro[key] = value;
+      });
+      req.libro.save((err) => {
+        if (err) {
           return res.send(err);
         }
         return res.json(libro);
       });
     })
-    .put((req,res) => {
-      Libro.findById(req.params.idLibro, (err,libro) =>{
+    .delete((req,res) => {
+      req.libro.remove((err) => {
         if(err){
           return res.send(err);
         }
-        libro.titulo = req.body.titulo;
-        libro.autor = req.body.autor;
-        libro.genero = req.body.genero;
-        libro.visto = req.body.visto;
-        libro.save();
-        return res.json(libro);
+        return res.sendStatus(204);
       });
     });
-
   return libroRouter; //salga de la funcion
 }
 
